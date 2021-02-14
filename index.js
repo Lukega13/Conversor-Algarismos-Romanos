2c28e09bc640a66a48894e98d5dabcb16a0a120a
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2021-02-14 17:09:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //1- Só permitir digitar I, V, X, L, C, D, M
 //2- Identificar o número digitado 
 //3- Ver se o proximo numero é maior, menor ou igual
-//4- Se for maior subtrai o proximo numero do anterior
+//4- Se for maior da ERRO
 //5- Se for menor ou igual soma o proximo numero ao atual
 
 var number = document.getElementById("roman-algarism-input")
@@ -13,8 +13,57 @@ number.addEventListener("keyup", identifyNumber)
 
 function identifyNumber() {
 
-    for (i = 0; i < number.value.length; i++) {
+    numero = number.value.toUpperCase()
+    calculo = 0
+
+
+    for (i = 0; i < numero.length; i++) {
+
+        if (numero[i] == 'I') {
+            resultado = 1
+        }
+
+        if (numero[i] == 'V') {
+            resultado = 5
+        }
+
+        if (numero[i] == 'X') {
+            resultado = 10
+        }
+
+        if (numero[i - 1] == 'I') {
+            resultado1 = 1
+        }
+
+        if (numero[i - 1] == 'V') {
+            resultado1 = 5
+        }
+
+        if (numero[i - 1] == 'X') {
+            resultado1 = 10
+        }
+
+
+        if (numero[i - 1] != undefined) {
+
+            if (numero[i] <= numero[i - 1]) {
+                calculo += resultado
+            }
+
+            if (numero[i] > numero[i - 1]) {
+                calculo += Math.abs(resultado - resultado1) - resultado1
+            }
+
+        }
+
+        if (numero[i - 1] == undefined) {
+
+            calculo += resultado
+
+        }
 
     }
+
+    result.value = calculo
 
 }
